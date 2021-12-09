021a0b16de3fe36e70a0874906ce28ee65b14272
Evandro <eesvans@hotmail.com>
2021-12-09 14:54:20 -0300

    Ajuste no código

--- JAVA_LacosDec/LacosDec_exerc_4.java
+++ JAVA_LacosDec/LacosDec_exerc_4.java
@@ -8,18 +8,15 @@ public class LacosDec_exerc_4 {
 		
 		Scanner leia = new Scanner(System.in);
 		
-		int num;
-		double numQuad;
-		
+		double num;
+				
 		System.out.print("Entre com um número: ");
 		num = leia.nextInt();
-		
-		numQuad = Math.pow(num, 2);
-		
+
 		if((num%2==0)) {
 			System.out.printf("O número "+num+" é par e sua raíz quadrada é %.2f",Math.sqrt(num));
 		} else {
-			System.out.println("O número "+num+" é ímpar e elevado ao quadrado é "+numQuad);
+			System.out.println("O número "+num+" é ímpar e elevado ao quadrado é "+Math.pow(num, 2));
 		}
 	}
 }
