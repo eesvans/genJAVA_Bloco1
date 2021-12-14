236f26824b5723266d89474f83ba9cd3ce8f0206
Evandro <eesvans@hotmail.com>
2021-12-14 09:51:32 -0300

    adição DO WHILE

--- Arrays/Array_ex4.java
+++ Arrays/Array_ex4.java
@@ -40,114 +40,121 @@ public class Array_ex4 {
 				m2[linha][coluna]=leia.nextFloat();
 			}
 		}
-			
+		
+		do
+		{
 		System.out.println("\nAgora que informou os valores, escolha uma das opções abaixo: ");
 		System.out.println("\n1 - Somar as duas matrizes.");
 		System.out.println("\n2 - Subtrair a primeira matriz da segunda.");
 		System.out.println("\n3 - Adicionar uma constante as duas matrizes.");
 		System.out.println("\n4 - Imprimir as matrizes.");
+		System.out.println("\n0 - Sair do sistema.");
 		op=leia.nextInt();
 		
 		switch(op)
-		{
-			case 1:
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+			{
+				case 1:
+					for(linha=0;linha<2;linha++)
 					{
-						m3[linha][coluna]=m1[linha][coluna]+m2[linha][coluna];
-						
+						for(coluna=0;coluna<2;coluna++)
+						{
+							m3[linha][coluna]=m1[linha][coluna]+m2[linha][coluna];
+							
+						}
 					}
-				}
-				System.out.println("\nA soma das duas matrizes é de: ");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					System.out.println("\nA soma das duas matrizes é de: ");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m3[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m3[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				break;
-				
-			case 2:
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					break;
+					
+				case 2:
+					for(linha=0;linha<2;linha++)
 					{
-						m3[linha][coluna]=m1[linha][coluna]-m2[linha][coluna];
-						
+						for(coluna=0;coluna<2;coluna++)
+						{
+							m3[linha][coluna]=m1[linha][coluna]-m2[linha][coluna];
+							
+						}
 					}
-				}
-				System.out.println("\nA subtração da primeira matriz para a segunda é de: ");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					System.out.println("\nA subtração da primeira matriz para a segunda é de: ");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m3[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m3[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				break;
-			case 3:
-				
-				System.out.println("\nInforme uma constante a adicionar na primeira e segunda matriz: ");
-				num=leia.nextFloat();
-				
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					break;
+				case 3:
+					
+					System.out.println("\nInforme uma constante a adicionar na primeira e segunda matriz: ");
+					num=leia.nextFloat();
+					
+					for(linha=0;linha<2;linha++)
 					{
-						m1[linha][coluna]+=num;
-						m2[linha][coluna]+=num;
-												
+						for(coluna=0;coluna<2;coluna++)
+						{
+							m1[linha][coluna]+=num;
+							m2[linha][coluna]+=num;
+													
+						}
 					}
-				}
-				
-								
-				System.out.println("\nNovos valores MATRIZ 1");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					
+									
+					System.out.println("\nNovos valores MATRIZ 1");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m1[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m1[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				System.out.println("\nNovos valores MATRIZ 2");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					System.out.println("\nNovos valores MATRIZ 2");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m2[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m2[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				
-				break;
-				
-			case 4:
-				System.out.println("\nMATRIZ 1 ");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					
+					break;
+					
+				case 4:
+					System.out.println("\nMATRIZ 1 ");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m1[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m1[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				System.out.println("\nMATRIZ 2 ");
-				for(linha=0;linha<2;linha++)
-				{
-					for(coluna=0;coluna<2;coluna++)
+					System.out.println("\nMATRIZ 2 ");
+					for(linha=0;linha<2;linha++)
 					{
-						System.out.printf("\t %.1f \t",m2[linha][coluna]);
+						for(coluna=0;coluna<2;coluna++)
+						{
+							System.out.printf("\t %.1f \t",m2[linha][coluna]);
+						}
+						System.out.println();
 					}
-					System.out.println();
-				}
-				break;
-				
-		}
+					break;
+					default:
+						System.out.println("\nOpção inválida ou sair do sistema.");
+			}
+		}while(op!=0);
+		
+		
 		
 			
 	}
