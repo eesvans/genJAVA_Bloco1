51beffd093f3b6047ccb8747569a5eb71cde2543
Evandro <eesvans@hotmail.com>
2021-12-18 23:23:29 -0300

    exclusão de atributo não utilizado

--- Projeto_G2/PjCinema.java
+++ Projeto_G2/PjCinema.java
@@ -6,12 +6,10 @@ public abstract class PjCinema {
 	Scanner leia = new Scanner(System.in);
 	
 	private int idade;
-	private String ingresso;
 	
-	public PjCinema(int idade, String ingresso)
+	public PjCinema(int idade)
 	{
 		this.idade=idade;
-		this.ingresso=ingresso;
 	}
 	
 	public PjCinema()
@@ -24,12 +22,6 @@ public abstract class PjCinema {
 	}
 	public void setIdade(int idade) {
 		this.idade = idade;
-	}
-	public String getIngresso() {
-		return ingresso;
-	}
-	public void setIngresso(String ingresso) {
-		this.ingresso = ingresso;
 	}
 	
 }
